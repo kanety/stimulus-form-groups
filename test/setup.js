234a7768ce65d8b75baccc85550be35bc4a065d4
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2024-06-26 11:44:07 +0900

    Fix test and examples

--- test/setup.js
+++ test/setup.js
@@ -1,8 +1,8 @@
-global.$ = document.querySelector.bind(document);
-global.$$ = document.querySelectorAll.bind(document);
-
 import { Application } from '@hotwired/stimulus';
 import FormGroupsController from 'index';
 
 const application = Application.start();
 application.register('form-groups', FormGroupsController);
+
+global.$ = document.querySelector.bind(document);
+global.$$ = document.querySelectorAll.bind(document);
